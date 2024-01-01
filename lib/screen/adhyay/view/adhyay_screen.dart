1ae4189b5cb6efc5eeb4c4d4823a94db36101f8c
Jeniss09 <jenishmevada1@gmail.com>
2024-01-01 21:59:41 +0530

    new 8

--- lib/screen/adhyay/view/adhyay_screen.dart
+++ lib/screen/adhyay/view/adhyay_screen.dart
@@ -18,99 +18,125 @@ class AdhyayScreen extends StatefulWidget {
 class _AdhyayScreenState extends State<AdhyayScreen> {
   AdhyayProvider? providerR;
   AdhyayProvider? providerW;
+
   @override
   Widget build(BuildContext context) {
-    HomeModel model = ModalRoute
-        .of(context)!
-        .settings
-        .arguments as HomeModel;
-    providerR=context.read<AdhyayProvider>();
-    providerW=context.watch<AdhyayProvider>();
+    HomeModel model = ModalRoute.of(context)!.settings.arguments as HomeModel;
+    providerR = context.read<AdhyayProvider>();
+    providerW = context.watch<AdhyayProvider>();
     return SafeArea(
-        child: Scaffold(
-            appBar: AppBar(
-              title: const Text("Bhagavad Gita"),
-              automaticallyImplyLeading: false,
-              actions: [
-                PopupMenuButton(
-                  itemBuilder: (context) {
-                    return [
-                      PopupMenuItem(child: RadioListTile(
-                        value: "All",
-                        groupValue: providerW!.language,
-                        onChanged: (value) {
-                          providerR!.setLanguage(value.toString());
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Bhagavad Gita"),
+          automaticallyImplyLeading: false,
+          actions: [
+            PopupMenuButton(
+              itemBuilder: (context) {
+                return [
+                  PopupMenuItem(
+                    child: RadioListTile(
+                      value: "All",
+                      groupValue: providerW!.language,
+                      onChanged: (value) {
+                        providerW!.setLanguage(value.toString());
+                      },
+                      title: const Text("All"),
+                    ),
+                  ),
+                  PopupMenuItem(
+                    child: RadioListTile(
+                      value: "Gujarati",
+                      groupValue: providerW!.language,
+                      onChanged: (value) {
+                        providerW!.setLanguage(value.toString());
+                      },
+                      title: const Text("Gujarati"),
+                    ),
+                  ),
+                  PopupMenuItem(
+                    child: RadioListTile(
+                      value: "Hindi",
+                      groupValue: providerW!.language,
+                      onChanged: (value) {
+                        providerW!.setLanguage(value.toString());
+                      },
+                      title: const Text("Hindi"),
+                    ),
+                  ),
+                  PopupMenuItem(
+                    child: RadioListTile(
+                      value: "English",
+                      groupValue: providerW!.language,
+                      onChanged: (value) {
+                        providerW!.setLanguage(value.toString());
+                      },
+                      title: const Text("English"),
+                    ),
+                  ),
+                  PopupMenuItem(
+                    child: Consumer<ThemeProvider>(
+                      builder: (context, value, child) => Switch(
+                        value: value.isLight,
+                        onChanged: (value1) {
+                          ShareHelper shr = ShareHelper();
+                          shr.setTheme(value1);
+                          value.changeTheme();
                         },
-                        title: const Text("All"),
-                      ),),
-                      PopupMenuItem(child: RadioListTile(
-                        value: "Gujarati",
-                        groupValue: providerW!.language,
-                        onChanged: (value) {
-                          providerR!.setLanguage(value.toString());
-                        },
-                        title: const Text("Gujarati"),
-                      ),),
-                      PopupMenuItem(child: RadioListTile(
-                        value: "Hindi",
-                        groupValue: providerW!.language,
-                        onChanged: (value) {
-                          providerR!.setLanguage(value.toString());
-                        },
-                        title: const Text("Hindi"),
-                      ),),
-                      PopupMenuItem(child: RadioListTile(
-                        value: "English",
-                        groupValue: providerW!.language,
-                        onChanged: (value) {
-                          providerR!.setLanguage(value.toString());
-                        },
-                        title: const Text("English"),
-                      ),),
-                      PopupMenuItem(
-                        child: Consumer<ThemeProvider>(
-                          builder: (context, value, child) =>
-                              Switch(
-                                value: value.isLight,
-                                onChanged: (value1) {
-                                  ShareHelper shr = ShareHelper();
-                                  shr.setTheme(value1);
-                                  value.changeTheme();
-                                },
-                              ),
-                        ),
                       ),
-                    ];
-                  },
+                    ),
+                  ),
+                ];
+              },
+            ),
+            const LikeButton(),
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/image/Bhagavad_Gita.jpg",
+                  height: 300,
+                  width: 350,
                 ),
-                const LikeButton(),
+                const Center(
+                  child: Text(" 🙏🙏🙏જય શ્રી કૃષ્ણા 🙏🙏🙏"),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                providerR!.language == 'All'
+                    ? Column(
+                        children: [
+                          Text(model.meaning,
+                              style: const TextStyle(fontSize: 20)),
+                          const SizedBox(height: 20,),
+                          Text(model.sholka,
+                              style: const TextStyle(fontSize: 20)),
+                          const SizedBox(height: 20,),
+                          Text(model.english,
+                              style: const TextStyle(fontSize: 20)),
+                        ],
+                      )
+                    : providerR!.language == "Gujarati"
+                        ? Text(model.meaning,
+                            style: const TextStyle(fontSize: 20))
+                        : providerR!.language == "Hindi"
+                            ? Text(model.sholka,
+                                style: const TextStyle(fontSize: 20))
+                            : Text(model.english,
+                                style: const TextStyle(fontSize: 20))
+                //             providerR!.language=='All'?Text(model.meaning, style: const TextStyle(fontSize: 20))
+                //         :providerR!.language=='Gujarati'?Text(model.sholka, style: const TextStyle(fontSize: 20)):
+                // Text(model.english, style: const TextStyle(fontSize: 20)),
               ],
             ),
-            body: SingleChildScrollView(
-                child: Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                      Image.asset(
-                      "assets/image/Bhagavad_Gita.jpg",
-                      height: 300,
-                      width: 350,
-                    ),
-                    const Center(
-                      child: Text(" 🙏🙏🙏જય શ્રી કૃષ્ણા 🙏🙏🙏"),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    providerR!.language=='All'?Text(model.meaning, style: const TextStyle(fontSize: 20))
-                :providerR!.language=='Gujarati'?Text(model.meaning, style: const TextStyle(fontSize: 20)):
-        Text(model.meaning, style: const TextStyle(fontSize: 20)),
-           ],
-         ),
+          ),
         ),
-       ),
-     ),
+      ),
     );
   }
 }
